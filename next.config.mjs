71d227b0e69a6f79767c18ff185d5337bc554374
themonsteredu <cuteheea0@gmail.com>
2026-04-27 15:37:34 +0900

    코드 되돌리기
    Removed async rewrites configuration from Next.js config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  async rewrites() {
-    return [
-      {
-        source: '/tree',
-        destination: 'https://monster-tree.vercel.app/tree',
-      },
-      {
-        source: '/tree/:path*',
-        destination: 'https://monster-tree.vercel.app/tree/:path*',
-      },
-    ];
-  },
 };
-
 export default nextConfig;
