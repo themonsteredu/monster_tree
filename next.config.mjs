3875192e3e0bd1eeb255162f6bcbdf63c68f4b48
themonsteredu <cuteheea0@gmail.com>
2026-04-27 15:36:18 +0900

    feat: /tree 경로를 monster-tree로 프록시 연결
    Added async rewrites for '/tree' routes to redirect to an external URL.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/tree',
+        destination: 'https://monster-tree.vercel.app/tree',
+      },
+      {
+        source: '/tree/:path*',
+        destination: 'https://monster-tree.vercel.app/tree/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
